a61a03f3acc3caa93d9e174f8d1852f2e436f0f3
Andrew Dinh <andrewdinh74@yahoo.com>
2015-06-24 23:30:17 -0700

    Remove some redundant lines from config.js

--- client/config/config.js
+++ client/config/config.js
@@ -6,9 +6,5 @@ angular.module('poseidon')
 
   $stateProvider
   .state('home', {url: '/', templateUrl: '/views/general/home.html', controller: 'HomeCtrl'})
-  .state('about', {url: '/about', templateUrl: '/views/general/about.html'})
-  .state('faq', {url: '/faq', templateUrl: '/views/general/faq.html'})
-  .state('contact', {url: '/contact', templateUrl: '/views/general/contact.html'})
-  .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'});
 });
